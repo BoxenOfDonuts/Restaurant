56221b2bbde52947afaf5ace5419387e1e039d91
Joel H <joel.hacke@gmail.com>
2020-12-06 11:51:48 -0600

    removed comments and uncessary code

--- src/menu.js
+++ src/menu.js
@@ -1,25 +1,6 @@
 import { divFactory, createText } from './utils.js'
 import { generateFooter } from './footer'
 
-// const generateContactText = () => {
-//     const parent = divFactory('text-content');
-//     const header = createText('Contact Us', 'h1');
-//     const lorem = createText('Lorem ipsum dolor sit amet, consectetur adipiscing elit,\
-//     sed do eiusmod tempor incididunt ut labore et dolore \
-//     magna aliqua. Ut enim ad minim veniam, quis nostrud \
-//     exercitation ullamco laboris nisi ut aliquip ex ea \
-//     commodo consequat. Duis aute irure dolor in \
-//     reprehenderit in voluptate velit esse cillum dolore eu\
-//     fugiat nulla pariatur. Excepteur sint occaecat cupidatat\
-//     non proident, sunt in culpa qui officia deserunt mollit\
-//     anim id est laborum.', 'p');
-
-//     parent.appendChild(header);
-//     parent.appendChild(lorem);
-
-//     return parent
-// }
-
 const menuItem = (name, desc, pic, price) => {
     return { name, desc, pic, price }
 }
@@ -29,9 +10,9 @@ const pictureUrl = '../dist/resources/images/mug-large.jpg'
 const Menu = (() => {
     let menuItems = []
 
-    const printItems = () => {
-        items().forEach((item) => console.log(item))
-    }
+    // const printItems = () => {
+    //     items().forEach((item) => console.log(item))
+    // }
 
     /* just to have a menu */
     const _fillMenu = () => {
@@ -73,11 +54,9 @@ const Menu = (() => {
 
     const _createMenuItemCard = (item) => {
         const gridItem = divFactory('grid-item')
-        // gridItem.classList.add('text-content')
         const text = divFactory('item-info')
         const picture = divFactory('item-picture')
 
-        console.log(item)
         for (let key in item) {
             let stuff = item[key]
             if (key === 'pic') {
@@ -89,19 +68,6 @@ const Menu = (() => {
             } else if (key === 'price') {
                 stuff = `$${item[key]}`
             }
-            // switch(key) {
-            //     case "pic":
-            //         const pic = document.createElement('img');
-            //         pic.alt = item.name;
-            //         pic.src = item[key];
-            //         picture.appendChild(pic);
-            //         continue;
-            //     case "price":
-            //         const price = `$${item[key]}`
-            //         break;
-            //         // element = createText(`${item[key]}`)
-            // }
-
             const div = divFactory(key)
             const content = createText(stuff, 'p')
             div.appendChild(content)
@@ -111,7 +77,6 @@ const Menu = (() => {
         gridItem.appendChild(text)
         gridItem.appendChild(picture)
 
-        console.log(gridItem)
         return gridItem
     }
 
@@ -121,7 +86,6 @@ const Menu = (() => {
             grid.appendChild(_createMenuItemCard(item))
         })
 
-        console.log(grid)
         return grid
     }
 
@@ -130,7 +94,6 @@ const Menu = (() => {
 
 const createMenuPage = () => {
     const parent = divFactory('image-text')
-    //parent.appendChild(generateContactText())
     parent.appendChild(generateFooter())
 
     return parent
